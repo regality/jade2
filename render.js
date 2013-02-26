753458379e8accf64fae18005238792be3f1000d
Adam Blackburn <regality@gmail.com>
2013-02-26 10:43:16 -0700

    use options as locals

--- render.js
+++ render.js
@@ -5,9 +5,7 @@ var fs      = require('fs')
 
 var options = {
   pretty: process.argv.indexOf('--pretty') !== -1,
-  locals: {
-    title: 'zomga jade!'
-  }
+  title: 'zomga jade!'
 };
 var html = jade.renderFile(process.argv[2], options);
 process.stdout.write(html);
